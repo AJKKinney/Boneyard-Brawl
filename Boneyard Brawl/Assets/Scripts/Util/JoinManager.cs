252e6c92098a67654a83543089d332bbf7cde40a
AJKKinney <AJKKinney@gmail.com>
2022-07-08 19:51:30 -0700

    Title Screen and Join Manager

--- Boneyard Brawl/Assets/Scripts/Util/JoinManager.cs
+++ Boneyard Brawl/Assets/Scripts/Util/JoinManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JoinManager : MonoBehaviour
 {
 
     public static JoinManager instance;
 
+    [SerializeField] private Button startButton;
+
     [SerializeField] private GameObject[] playerBlackouts;
+
 
     // Initialize
     void Awake()
@@ -28,6 +32,15 @@ public class JoinManager : MonoBehaviour
         if (index <= playerBlackouts.Length - 1)
         {
             playerBlackouts[index].SetActive(false);
+        }
+
+        if(index < 1)
+        {
+            startButton.interactable = false;
+        }
+        else
+        {
+            startButton.interactable = true;
         }
     }
 
